b8528208f928e730f6356701caba5c82f8f0d22c
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-03 03:57:34 -0700

    Make main classes final for Cobblemon Torchmaster Fix

--- fabric/src/main/java/dev/matthiesen/cobblemon_torchmaster_fix/fabric/CobblemonTorchMasterFixFabric.java
+++ fabric/src/main/java/dev/matthiesen/cobblemon_torchmaster_fix/fabric/CobblemonTorchMasterFixFabric.java
@@ -3,7 +3,7 @@ package dev.matthiesen.cobblemon_torchmaster_fix.fabric;
 import dev.matthiesen.cobblemon_torchmaster_fix.common.CobblemonTorchMasterFixCommon;
 import net.fabricmc.api.ModInitializer;
 
-public class CobblemonTorchMasterFixFabric implements ModInitializer {
+public final class CobblemonTorchMasterFixFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         var instance = CobblemonTorchMasterFixCommon.INSTANCE;
